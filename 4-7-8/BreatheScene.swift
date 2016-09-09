ab717ef1a72352e5b793708eb7d76408b9e7f64e
davidvypark <davidvypark@gmail.com>
2016-09-09 16:20:48 -0400

    added SKScene will try Spritekit for the animations

---
+++ 4-7-8/BreatheScene.swift
@@ -0,0 +1,27 @@
+//
+//  BreatheScene.swift
+//  4-7-8
+//
+//  Created by David Park on 9/9/16.
+//  Copyright © 2016 DavidVY. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+import GameplayKit
+
+
+class BreatheScene: SKScene {
+	
+	let box = SKSpriteNode()
+	
+	override func didMoveToView(view: SKView) {
+		
+		backgroundColor = SKColor.peterRiverColor()
+	}
+	
+	
+	
+	
+	
+}
