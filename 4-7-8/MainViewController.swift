97ce77511af1f722aad1572b0f1d5cabd9e509d9
davidvypark <davidvypark@gmail.com>
2016-09-09 13:14:23 -0400

    wireframe for sequence

--- 4-7-8/MainViewController.swift
+++ 4-7-8/MainViewController.swift
@@ -8,16 +8,40 @@
 
 import UIKit
 import SpriteKit
+import SnapKit
 
 class MainViewController: UIViewController {
+	
+	var box = UIView()
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		setupScene()
+		
+		startSequence()
 	}
 	
 	func setupScene() {
 		view.backgroundColor = UIColor.peterRiverColor()
+		
+		view.addSubview(box)
+		box.snp_makeConstraints { (make) in
+			make.width.equalTo(view.snp_width).dividedBy(10)
+			make.height.equalTo(view.snp_width).dividedBy(10)
+			
+		}
+		
+	}
+	
+	func startSequence() {
+		
+		//4 seconds in
+		
+		
+		//7 seconds hold
+		
+		
+		//8 seconds out
 	}
 	
 	
