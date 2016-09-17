769ee9362072d5cbd641406e3e0e781f334a8d3a
davidvypark <davidvypark@gmail.com>
2016-09-17 13:56:00 -0500

    created pulsar for breathe sequence

--- 4-7-8/BreatheScene.swift
+++ 4-7-8/BreatheScene.swift
@@ -14,6 +14,7 @@ import GameplayKit
 class BreatheScene: SKScene {
 	
 	var box = SKSpriteNode()
+	var circle = SKShapeNode(circleOfRadius: 40)
 	
 	var startPoint = CGPoint()
 	var endPoint = CGPoint()
@@ -21,27 +22,48 @@ class BreatheScene: SKScene {
 	
 	override func didMoveToView(view: SKView) {
 		
-		startPoint = CGPoint(x: view.frame.height / 10, y: view.frame.width / 2)
-		endPoint = CGPoint(x: view.frame.height / 1.2, y: view.frame.width / 2)
+//		startPoint = CGPoint(x: view.frame.height / 10, y: view.frame.width / 2)
+//		endPoint = CGPoint(x: view.frame.height / 1.2, y: view.frame.width / 2)
 		
 		backgroundColor = SKColor.whiteColor()
-		box = SKSpriteNode(color: UIColor.peterRiverColor(), size: CGSizeMake(20, 20))
+		box = SKSpriteNode(color: UIColor.belizeHoleColor(), size: CGSizeMake(20, 20))
 		box.position = startPoint
 		
-		animate()
+		let centerScreen = CGPoint(x: view.frame.width / 2, y: view.frame.height / 2)
+		circle.fillColor = UIColor.blueColor()
+		
+		circle.position = centerScreen
+		addChild(circle)
+		
+		animateCircle()
 	}
 	
-	func animate() {
+//	func animate() {
+//		
+//		let fourAction = SKAction.moveTo(CGPoint(x: 50, y: 50), duration: 4)
+//		let sevenAction = SKAction.moveTo(CGPoint(x: 200, y: 500), duration: 7)
+//		let eightAction = SKAction.moveTo(CGPoint(x: 50, y: 500 ), duration: 8)
+//		
+//		let sequence = SKAction.sequence([fourAction, sevenAction, eightAction])
+//		
+//		box.runAction(SKAction.repeatActionForever(sequence))
+//		addChild(box)
+//	}
 	
+	func animateCircle() {
 		
-		let fourAction = SKAction.moveTo(CGPoint(x: 50, y: 50), duration: 4)
-		let sevenAction = SKAction.moveTo(CGPoint(x: 200, y: 500), duration: 7)
-		let eightAction = SKAction.moveTo(CGPoint(x: 50, y: 500 ), duration: 8)
+		let growAction = SKAction.scaleBy(2, duration: 4)
+		let waitAction = SKAction.waitForDuration(7)
+		let shrinkAction = SKAction.scaleBy(0.5, duration: 8)
 		
-		let sequence = SKAction.sequence([fourAction, sevenAction, eightAction])
+		let sequence = SKAction.sequence([growAction, waitAction, shrinkAction])
 		
-		box.runAction(SKAction.repeatActionForever(sequence))
-		addChild(box)
+		circle.runAction(SKAction.repeatActionForever(sequence))
+		
+	}
+	
+	func animateBar() {
+		
 	}
 	
 
